f3b09e960813d86e72ecfbc09a41059d0f7f2ded
archiechop1 <Allar_tahe@outlook.com>
2023-02-14 11:21:38 +0200

    add burger menu click and dropdown
    todo: header fixed

--- js/script.js
+++ js/script.js
@@ -1,2 +1,23 @@
-jQuery("p").css("color", "blue");
-console.log("test test123")
+jQuery(document).ready(function() {
+
+    jQuery(".burger-menu").click(function() {
+        jQuery(this).siblings(".fixed-sidebar").addClass("active");
+    });
+    jQuery(".close-button").click(function() {
+        jQuery(this).closest(".fixed-sidebar").removeClass("active");
+    });
+
+    jQuery(".menu__has-children").hover(function() {
+        jQuery(this).children(".menu__dropdown").stop().slideToggle();
+    });
+
+
+    jQuery(window).scroll(function(event) {
+
+        jQuery("header.fixed").slideDown();
+
+
+
+    });
+
+});
